2422b47bfd747544ab7cf5c51745122600c5f902
Srikanth <srikanthjviru@gmail.com>
2021-06-10 19:23:25 -0400

    added new api

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -5,6 +5,10 @@ router.get("/test", async (req, res) => {
 	res.json({ message: "GET call response" });
 	return;
 });
+router.get("/test1", async (req, res) => {
+	res.json({ message: "GET TEST 1 call response" });
+	return;
+});
 router.post("/feedback", async (req, res, next) => {
 	const feedback = req.body.feedback;
 	console.log(feedback);
